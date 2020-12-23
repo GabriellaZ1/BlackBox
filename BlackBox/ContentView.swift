02f34af8a00d963b18640401a25b4f341acd8a6f
GabriellaZ1 <76516410+GabriellaZ1@users.noreply.github.com>
2020-12-23 11:22:00 +0100

    Initial Commit

---
+++ BlackBox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlackBox
+//
+//  Created by zeoligabriella on 23/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
